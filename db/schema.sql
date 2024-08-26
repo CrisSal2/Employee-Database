877181fa8a216cc89b98eae06c4e8817a87da447
CrisSal2 <salgado.chris.m@gmail.com>
2024-08-25 18:42:16 -0700

    added seeds and npm packages

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,16 @@
+
+-- Creates database and connects to it
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 \c employee_db;
 
-CREATE TABLE employee (
+
+CREATE TABLE department (
     id SERIAL NOT NULL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    name VARCHAR(30)
 );
+
 
 CREATE TABLE role (
     id SERIAL NOT NULL PRIMARY KEY,
@@ -23,7 +22,12 @@ CREATE TABLE role (
     ON DELETE CASCADE
 );
 
-CREATE TABLE department (
+
+CREATE TABLE employee (
     id SERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(30)
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
 );
---
+++ seeds/seeds.sql
@@ -0,0 +1,29 @@
+-- Comment on the right is the automatic department id it's given
+INSERT INTO department (name) VALUES 
+('Sales'), -- 1
+('Engineering'), -- 2
+('Finance'), -- 3
+('Leagal'), -- 4
+
+-- Comment on the right is the automatic role id it's given
+INSERT INTO role (title, salary, department_id) VALUES 
+('Sales Lead', 80000, 1), -- 1
+('Salesperson', 50000, 1), -- 2
+('Lead Engineer', 100000, 2), -- 3
+('Software Engineer', 80000, 2), -- 4
+('Account Manager', 90000, 3), -- 5
+('Accountant', 75000, 3), --6
+('Leagal Team Lead', 120000, 4), -- 7
+('Lawyer', 95000, 4); -- 8
+
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES
+('Cris', 'Salgado', 4),
+('Kate', 'Salgado', 3),
+('Leanne', 'Salgado', 5),
+('David', 'Haig', 6),
+('Kevin', 'Marx', 1),
+('Jake', 'Hamlin', 2),
+('Abinadi', 'Cruz', 7),
+('Nathaniel', 'Spence', 8),
+('Genji', 'Shimada', 4);
